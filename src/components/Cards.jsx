ee12d391694b91e06a9d11061a249db5b6d446d0
Sanjero <gelosantos72@gmail.com>
2022-12-30 22:42:33 +0800

    Add card animation on click

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
 function Cards({ characters, clickCard }) {
+  const cardAnimation = () => {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach((card) => {
+      card.classList.add('minimize');
+      setTimeout(() => {
+        card.classList.remove('minimize');
+      }, 100);
+    });
+  };
+
   const renderCards = () => {
     if (!characters) return;
     return characters.map((character) => (
       <div
         className="card"
         key={character.name}
-        onClick={(e) => clickCard(e, character)}
+        onClick={(e) => {
+          clickCard(e, character);
+          cardAnimation();
+        }}
       >
         <img className="card-img" src={character.img} alt="" />
       </div>
