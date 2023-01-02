19ea68789c84fa26cc87fea9d6ac5c7cb9acd38f
Sanjero <gelosantos72@gmail.com>
2023-01-02 21:59:46 +0800

    Refactor keys for card mapping function
    Instead of using character name as its key, I randomize the keys so that
    it will unmount and remount each individual card so it will show the
    zoom in animation every click.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
 
 function Cards({ characters, clickCard }) {
-  const cardAnimation = () => {
-    const cards = document.querySelectorAll('.card');
-
-    cards.forEach((card) => {
-      card.classList.add('minimize');
-      setTimeout(() => {
-        card.classList.remove('minimize');
-      }, 100);
-    });
-  };
-
   const renderCards = () => {
     if (!characters) return;
     return characters.map((character) => (
       <div
         className="card"
-        key={character.name}
-        onClick={(e) => {
-          clickCard(e, character);
-          cardAnimation();
+        key={Math.random()}
+        onClick={() => {
+          clickCard(character);
         }}
       >
         <img className="card-img" src={character.img} alt="" />
@@ -28,7 +16,12 @@ function Cards({ characters, clickCard }) {
     ));
   };
 
-  return <div className="card-container">{renderCards()}</div>;
+  return (
+    <div className="card-container">
+      {renderCards()}
+      {/*  */}
+    </div>
+  );
 }
 
 export default Cards;
